1f6836c8dab3fb57de0f65fbe1eb8465825bd2be
johan <johan@Jodacoga>
2022-05-04 11:37:46 -0500

    Creacion de las entidades cuenta,Tarjeta,tipo,usuario y abstractFacade, con sus respectivos atributos.

---
+++ src/main/java/ec/edu/ups/entidades/TipoUsuario.java
@@ -0,0 +1,34 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.ups.entidades;
+
+import jakarta.persistence.Entity;
+
+/**
+ *
+ * @author johan
+ */
+@Entity
+public class TipoUsuario {
+    
+    private String descripcion;
+
+    public TipoUsuario() {
+    }
+
+    public TipoUsuario(String descripcion) {
+        this.descripcion = descripcion;
+    }
+
+    public String getDescripcion() {
+        return descripcion;
+    }
+
+    public void setDescripcion(String descripcion) {
+        this.descripcion = descripcion;
+    }
+    
+    
+}
