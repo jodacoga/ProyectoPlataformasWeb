a06ec09c65eca7533aadcf004c359900af8a4331
Darwin <dleont@est.ups.edu.ec>
2022-05-19 00:35:14 -0500

    Update AbstractFacade

--- src/main/java/ec/edu/ups/facade/AbstractFacade.java
+++ src/main/java/ec/edu/ups/facade/AbstractFacade.java
@@ -5,33 +5,43 @@
 package ec.edu.ups.facade;
 
 import jakarta.persistence.EntityManager;
+import jakarta.persistence.criteria.CriteriaQuery;
+import java.util.List;
 
 /**
  *
  * @author johan
  */
-public abstract class AbstractFacade <T>{
+public abstract class AbstractFacade<T> {
+
     private Class<T> entityClass;
 
     public AbstractFacade(Class<T> entityClass) {
         this.entityClass = entityClass;
     }
-    
+
     protected abstract EntityManager getEntityManager();
-    
-    public void create(T entity){
+
+    public void create(T entity) {
         getEntityManager().persist(entity);
     }
-    
-    public void edit(T entity){
+
+    public void edit(T entity) {
         getEntityManager().merge(entity);
-        
+
     }
-    
-    public void remove (T entity){
+
+    public void remove(T entity) {
         getEntityManager().remove(entity);
     }
-     public T find(Object id) {
-    	return getEntityManager().find(entityClass, id);
+
+    public T find(Object id) {
+        return getEntityManager().find(entityClass, id);
+    }
+
+    public List<T> findAll() {
+        CriteriaQuery cq = getEntityManager().getCriteriaBuilder().createQuery();
+        cq.select(cq.from(entityClass));
+        return getEntityManager().createQuery(cq).getResultList();
     }
 }
