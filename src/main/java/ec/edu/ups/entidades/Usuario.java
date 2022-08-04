0d2ea794afabf5eaa51b102f820304965d257992
johan <johan@Jodacoga>
2022-08-04 00:43:56 -0500

    cambio tabla usuario

--- src/main/java/ec/edu/ups/entidades/Usuario.java
+++ src/main/java/ec/edu/ups/entidades/Usuario.java
@@ -26,6 +26,7 @@ import java.util.List;
  * @author johan
  */
 @Entity
+@Table(name="Usuario")
 public class Usuario implements Serializable {
 
     private static final long serialVersionUID = 1L;
