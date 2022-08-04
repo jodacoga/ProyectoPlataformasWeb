2fdef53db8da665bb28ded28f665830ad924eed9
johan <johan@Jodacoga>
2022-08-04 01:31:53 -0500

    Se cambio de nombre al categoria producto

--- src/main/java/ec/edu/ups/entidades/CategoriaProducto.java
+++ src/main/java/ec/edu/ups/entidades/CategoriaProducto.java
@@ -22,7 +22,7 @@ import java.util.List;
  * @author johan
  */
 @Entity
-@Table(name = "CategoriaFacade")
+@Table(name = "CategoriaProducto")
 public class CategoriaProducto implements Serializable {
 
     private static final long serialVersionUID = 1L;
